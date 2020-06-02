54f2dc32088985710a1c5d376d778e79a73879e1
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-06-02 20:10:33 +0900

    db-006のSQL文動作確認完了。

---
+++ db/db-challenge3/db-006/db-006.sql
@@ -0,0 +1,6 @@
+SELECT 
+  id, email AS メールアドレス, password AS パスワード, user_name AS ユーザ名,
+  self_introduction AS 自己紹介文, mobile_number AS 個人携帯電話番号,
+  company_tel AS 勤務先電話番号, created_at AS 作成日時, created_by AS 作成者ID,
+  updated_at AS 更新日時, updated_by AS 更新者ID, delete_flag AS 削除フラグ
+FROM users;
