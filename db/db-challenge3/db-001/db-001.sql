dec7600ddccc2b801c89a72eef17213a6a02f5a7
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-06-02 11:59:24 +0900

    FIRST PUSH

---
+++ db/db-challenge3/db-001/db-001.sql
@@ -0,0 +1,3 @@
+INSERT INTO users
+  (email, password, user_name, self_introduction, company_tel, mobile_number, delete_flag, created_by, created_at, updated_by, updated_at)
+  VALUES ('admin@labot.inc', 'admin', 'admin', 'このスペースの管理者です。', '00-0000-0000', NULL, 0, 1, '2020-04-20 15:47:52', 1, '2020-04-20 15:47:52');
---
+++ db/db-challenge3/db-002/db-002.sql
@@ -0,0 +1,3 @@
+INSERT INTO users
+  (email, password, user_name, self_introduction, company_tel, mobile_number, delete_flag, created_by, created_at, updated_by, updated_at)
+  VALUES('mysql@labot.inc', 'mysql55', 'mysql', NULL, NULL, '080-1234-1253', 0, 2, '2020-04-22 16:02:55', 2, '2020-04-22 16:02:55');
