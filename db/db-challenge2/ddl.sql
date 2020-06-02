fd9d4145847e515ae2fbaba6be4c01ad18951475
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-06-02 18:09:28 +0900

    同様にDDL文を修正。また、外部キーの場合もNOT NULL制約を指定。

--- db/db-challenge2/ddl.sql
+++ db/db-challenge2/ddl.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  email VARCHAR(100) NOT NULL UNIQUE,
+  email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   user_name VARCHAR(100) NOT NULL,
   self_introduction VARCHAR(1000),
   mobile_number CHAR(13),
-  company_tel CHAR(12),
+  company_tel CHAR(13),
   created_at DATETIME NOT NULL,
   created_by INTEGER NOT NULL,
   updated_at DATETIME NOT NULL,
@@ -16,7 +16,8 @@ CREATE TABLE users (
 CREATE TABLE chat_members (
   user_id INTEGER REFERENCES users (id),
   chat_room_id INTEGER REFERENCES chat_rooms (id),
-  joined_at DATETIME NOT NULL
+  joined_at DATETIME NOT NULL,
+  PRIMARY KEY (user_id, chat_room_id)
 );
 
 CREATE TABLE chat_rooms (
@@ -26,34 +27,34 @@ CREATE TABLE chat_rooms (
   file_permission INTEGER DEFAULT 0 NOT NULL,
   private_flag INTEGER DEFAULT 0 NOT NULL,
   created_at DATETIME NOT NULL,
-  created_by INTEGER REFERENCES users (id),
+  created_by INTEGER NOT NULL REFERENCES users (id),
   updated_at DATETIME NOT NULL,
-  updated_by INTEGER REFERENCES users (id),
+  updated_by INTEGER NOT NULL REFERENCES users (id),
   delete_flag INTEGER DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE posts (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  chat_room_id INTEGER REFERENCES chat_rooms (id),
+  chat_room_id INTEGER NOT NULL REFERENCES chat_rooms (id),
   contents VARCHAR(1000) NOT NULL,
   file_name VARCHAR(100),
   created_at DATETIME NOT NULL,
-  created_by INTEGER REFERENCES users (id),
+  created_by INTEGER NOT NULL REFERENCES users (id),
   updated_at DATETIME NOT NULL,
-  updated_by INTEGER REFERENCES users (id),
+  updated_by INTEGER NOT NULL REFERENCES users (id),
   delete_flag INTEGER DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE tasks (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  chat_room_id INTEGER REFERENCES chat_rooms (id),
+  chat_room_id INTEGER NOT NULL REFERENCES chat_rooms (id),
   task_detail VARCHAR(1000) NOT NULL,
   assigned_to INTEGER NOT NULL REFERENCES users (id),
   due_by DATETIME,
   complete_flag INTEGER DEFAULT 0 NOT NULL,
   created_at DATETIME NOT NULL,
-  created_by INTEGER REFERENCES users (id),
+  created_by INTEGER NOT NULL REFERENCES users (id),
   updated_at DATETIME NOT NULL,
-  updated_by INTEGER REFERENCES users (id),
+  updated_by INTEGER NOT NULL REFERENCES users (id),
   delete_flag INTEGER DEFAULT 0 NOT NULL
 );
